d33f46b0ab160d5e82a21f19fbb8d0adeb4c573c
yolavolaropa <39884472+yolavolaropa@users.noreply.github.com>
2020-10-11 03:06:36 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("COINGECKO", "GECKO", 18) {
+    constructor () public ERC20Detailed("COINGEKO", "GECKO", 18) {
         _mint(msg.sender, 30000 * (10 ** uint256(decimals())));
     }
 }
