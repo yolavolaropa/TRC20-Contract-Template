784b6d9e427d04676b2d0e1fa902df7f211cb579
yolavolaropa <39884472+yolavolaropa@users.noreply.github.com>
2020-10-11 03:33:24 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,22 +1,26 @@
+@ -1,22 
 // 0.5.1-c8a2
-// Enable optimization
-pragma solidity ^0.5.10;
+// Enable optimization	
+pragma solidity ^0.5.0;	
 
-import "./TRC20.sol";
-import "./TRC20Detailed.sol";
+
+import "./ERC20.sol";	import ".
+import "./ERC20Detailed.sol";	
+
 
 /**
- * @title GekoToken
- * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `TRC20` functions.
- */
-contract Token is TRC20, TRC20Detailed {
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.	 
+ * Note they can later distribute these tokens as they wish using `transfer` and other	 
+ * `ERC20` functions.	 
+ */	 
+contract Token is ERC20, ERC20Detailed {	
 
-    /**
+
+    /**	    
      * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    constructor () public TRC20Detailed("COINGEKO", "GECKO", 18) {
-        _mint(msg.sender, 30000 * (10 ** uint256(decimals())));
-    }
-}
+     */	     */
+    constructor () public ERC20Detailed("COINGEKO", "GECKO", 18) {	    
+        _mint(msg.sender, 30000 * (10 ** uint256(decimals())));	        
+    }	    
+} 
